9393b5d9c5a14056f8caa0caf1f7cfc12f4ea5cf
jon mananghaya <jon_mananghaya@dlsu.edu.ph>
2024-02-29 08:03:54 +0800

    Adjsuted the states to accomodate the start of the system. There is one new state

--- src/stateMapper.js
+++ src/stateMapper.js
@@ -11,8 +11,23 @@ stateMap.set(0, {
     "sol1": 0,
     "sol2": 0,
     "sol3": 0,
+    "sol4": 0,
 });
 stateMap.set(1, {
+    "pump0": 0,
+    "pump1": 0,
+    "mpump0": 0,
+    "mpump1": 0,
+    "mpump2": 0,
+    "aer": 0,
+    "rmixer": 0,
+    "sol0": 0,
+    "sol1": 0,
+    "sol2": 0,
+    "sol3": 0,
+    "sol4": 1,
+});
+stateMap.set(2, {
     "pump0": 0,
     "pump1": 0,
     "mpump0": 0,
@@ -24,8 +39,9 @@ stateMap.set(1, {
     "sol1": 0,
     "sol2": 0,
     "sol3": 0,
+    "sol4": 0,
 });
-stateMap.set(2, {
+stateMap.set(3, {
     "pump0": 0,
     "pump1": 0,
     "mpump0": 0,
@@ -37,8 +53,9 @@ stateMap.set(2, {
     "sol1": 0,
     "sol2": 0,
     "sol3": 0,
+    "sol4": 0,
 });
-stateMap.set(3, {
+stateMap.set(4, {
     "pump0": 1,
     "pump1": 0,
     "mpump0": 0,
@@ -50,8 +67,9 @@ stateMap.set(3, {
     "sol1": 0,
     "sol2": 0,
     "sol3": 0,
+    "sol4": 0,
 });
-stateMap.set(4, {
+stateMap.set(5, {
     "pump0": 0,
     "pump1": 0,
     "mpump0": 1,
@@ -63,8 +81,9 @@ stateMap.set(4, {
     "sol1": 0,
     "sol2": 0,
     "sol3": 0,
+    "sol4": 0,
 });
-stateMap.set(5, {
+stateMap.set(6, {
     "pump0": 0,
     "pump1": 0,
     "mpump0": 0,
@@ -76,9 +95,10 @@ stateMap.set(5, {
     "sol1": 0,
     "sol2": 0,
     "sol3": 0,
+    "sol4": 0,
 });
 
-stateMap.set(6, {
+stateMap.set(7, {
     "pump0": 0,
     "pump1": 0,
     "mpump0": 0,
@@ -90,8 +110,9 @@ stateMap.set(6, {
     "sol1": 0,
     "sol2": 0,
     "sol3": 0,
+    "sol4": 0,
 });
-stateMap.set(7, {
+stateMap.set(8, {
     "pump0": 0,
     "pump1": 0,
     "mpump0": 0,
@@ -103,19 +124,7 @@ stateMap.set(7, {
     "sol1": 1,
     "sol2": 0,
     "sol3": 0,
-});
-stateMap.set(8, {
-    "pump0": 0,
-    "pump1": 0,
-    "mpump0": 0,
-    "mpump1": 0,
-    "mpump2": 0,
-    "aer": 0,
-    "rmixer": 0,
-    "sol0": 0,
-    "sol1": 0,
-    "sol2": 0,
-    "sol3": 0,
+    "sol4": 0,
 });
 stateMap.set(9, {
     "pump0": 0,
@@ -127,10 +136,25 @@ stateMap.set(9, {
     "rmixer": 0,
     "sol0": 0,
     "sol1": 0,
-    "sol2": 1,
-    "sol3": 1,
+    "sol2": 0,
+    "sol3": 0,
+    "sol4": 0,
 });
 stateMap.set(10, {
+    "pump0": 0,
+    "pump1": 0,
+    "mpump0": 0,
+    "mpump1": 0,
+    "mpump2": 0,
+    "aer": 0,
+    "rmixer": 0,
+    "sol0": 0,
+    "sol1": 0,
+    "sol2": 1,
+    "sol3": 1,
+    "sol4": 0,
+});
+stateMap.set(11, {
     "pump0": 0,
     "pump1": 1,
     "mpump0": 0,
@@ -142,8 +166,9 @@ stateMap.set(10, {
     "sol1": 0,
     "sol2": 0,
     "sol3": 0,
+    "sol4": 0,
 });
-stateMap.set(11, {
+stateMap.set(12, {
     "pump0": 0,
     "pump1": 0,
     "mpump0": 0,
@@ -155,6 +180,7 @@ stateMap.set(11, {
     "sol1": 0,
     "sol2": 0,
     "sol3": 0,
+    "sol4": 0,
 });
 
 export default stateMap;
